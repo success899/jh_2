36ba892d76971a1d72113296f972a592819fc23e
jh_2 <success899@naver.com>
2021-12-13 13:40:50 +0900

    Account Login template view create

--- accountapp/static/js/login.js
+++ accountapp/static/js/login.js
@@ -1,6 +1,6 @@
 
 function send_input() {
-    axios.post('/accounts/create/', {
+    axios.post('/accounts/login/', {
         username: document.getElementById('username').value,
         password: document.getElementById('password').value,
       })
@@ -8,14 +8,16 @@ function send_input() {
         // POST 요청이 성공했을 경우 어떤 javascript 가 구동되어야 하는지에 대한 부분
         console.log(response);
 
-        document.getElementById('alert_box').innerHTML = "<div class='btn btn-primary rounded-pill px-5'>가입이 성공했습니다.</div>";
+        document.getElementById('alert_box').innerHTML = "<div class='btn btn-primary rounded-pill px-5'>로그인이 성공했습니다.</div>";
+
+        window.location.href = '/accounts/hello_world_template/';
 
       })
       .catch(function (error) {
         // POST 요청이 실패했을 경우 어떤 javascript 가 구동되어야 하는지에 대한 부분
         console.log(error);
 
-        document.getElementById('alert_box').innerHTML = "<div class='btn btn-danger rounded-pill px-5'>가입이 실패했습니다.</div>"
+        document.getElementById('alert_box').innerHTML = "<div class='btn btn-danger rounded-pill px-5'>로그인이 실패했습니다.</div>"
       });
 }
 
