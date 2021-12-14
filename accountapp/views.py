91c18d72d659ff0cd25532ef0de6a65d124f7700
jh_2 <success899@naver.com>
2021-12-14 10:36:17 +0900

    Update API template update

--- accountapp/views.py
+++ accountapp/views.py
@@ -64,6 +64,9 @@ class AccountRetrieveAPIView(RetrieveAPIView):
     permission_classes = [permissions.AllowAny]
     authentication_classes = [TokenAuthentication]
 
+class AccountUpdateTemplateView(TemplateView):
+    template_name = 'accountapp/update.html'
+
 class AccountUpdateAPIView(UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserWithoutPasswordSerializer
