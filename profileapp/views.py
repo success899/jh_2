d48347d931540c9ca0e20f99a6feefc08942f17c
jh_2 <success899@naver.com>
2021-12-22 02:26:11 +0900

    profile update api view update

--- profileapp/views.py
+++ profileapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from profileapp.models import Profile
@@ -24,3 +24,11 @@ class ProfileCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class ProfileUpdateAPIView(UpdateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    permission_classes = []
+    authentication_classes = [TokenAuthentication]
