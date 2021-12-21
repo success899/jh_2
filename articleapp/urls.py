adf031081c4fe9fefea8875afaa326c642543e59
jh_2 <success899@naver.com>
2021-12-22 02:51:52 +0900

    article create view update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,10 @@
+from django.urls import path
 
+from articleapp.views import ArticleCreateAPIView
+
+app_name = 'articleapp'
 
 urlpatterns = [
 
-]
+    path('', ArticleCreateAPIView.as_view(), name='create'),
+] 
